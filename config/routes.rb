6b7006c596182ac615a51aa075f1ded0a8335c37
Metsanpeitto <abraham.asturies@gmail.com>
2021-09-19 03:21:26 +0200

    Add alerts and notices

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'welcome#index'
+  get 'my_portfolio', to: 'users#my_portfolio' # #my_portfolio is an action
+  get 'search_stock', to: 'stocks#search'
 end
