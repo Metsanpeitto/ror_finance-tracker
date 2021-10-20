841de16412849c974db5776afd17a9eae1e3e83a
Metsanpeitto <abraham@waldenberginc.com>
2021-10-20 20:17:57 +0200

    First commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,3 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root 'welcome#index'
-  get 'my_portfolio', to: 'users#my_portfolio' # #my_portfolio is an action
-  get 'search_stock', to: 'stocks#search'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
