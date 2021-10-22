ea0336ea7aa527ca8e4729134aedfca0a33fb3a0
Metsanpeitto <abraham@waldenberginc.com>
2021-10-23 00:48:44 +0200

    Add friends relations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_many :user_stocks
   has_many :stocks, through: :user_stocks
+  has_many :friendships
+  has_many :friends, through: :friendships
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -22,7 +24,8 @@ class User < ApplicationRecord
   end
 
   def full_name
-    "#{first_name} #{last_name}" if first_name || last_name
+    return "#{first_name} #{last_name}" if first_name || last_name
+
     'Anonymous'
   end
 end
